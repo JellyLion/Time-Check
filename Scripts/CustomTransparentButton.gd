8e8cfe989de1bfd0f1f739dfe102f250ed9b7971
JellyLion <jellylionstudios@gmail.com>
2024-05-19 16:00:37 -0600

    Added title, another panel and imported scripts

---
+++ Scripts/CustomTransparentButton.gd
@@ -0,0 +1,31 @@
+extends TextureButton
+#
+#
+#func _ready() -> void:
+	#connect("mouse_entered", self, "mouse_entered")
+	#connect("mouse_exited", self, "mouse_exited")
+	#connect("toggled", self, "on_toggled")
+	#update_colours()
+	#
+	#
+#func update_colours() -> void:
+	#if pressed:
+		#modulate = Defaults.btn_active_colour
+	#else:
+		#modulate = Defaults.btn_inactive_colour
+	#
+	#
+#func mouse_entered() -> void:
+	#modulate = Defaults.btn_active_colour
+	#
+	#
+#func mouse_exited() -> void:
+	#if !pressed:
+		#modulate = Defaults.btn_inactive_colour
+#
+#
+#func on_toggled(really : bool) -> void:
+	#if really:
+		#modulate = Defaults.btn_active_colour
+	#else:
+		#modulate = Defaults.btn_inactive_colour
