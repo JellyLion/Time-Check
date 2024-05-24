b1248722d64c07ce0ee13263b540fdc150484c85
JellyLion <jellylionstudios@gmail.com>
2024-05-24 16:09:54 -0600

    Changed methods to main.gd, added functionalities
    Added a spinbox to set the minutes to call notification and also added a check button for starting counting minutes at current time.

---
+++ Main.gd
@@ -0,0 +1,33 @@
+extends Control
+
+var minutes_to_call_notification := 1
+var current_minute : int
+var start_at_current_time := false
+var minutes_past_starting_minute : int
+
+signal call_time_checker_notification
+
+func _on_current_time_minute_changed(minute):
+	current_minute = minute
+	if not start_at_current_time:
+		if current_minute % minutes_to_call_notification == 0:
+			emit_signal("call_time_checker_notification")
+	else:
+		minutes_past_starting_minute += 1
+		if minutes_past_starting_minute == 61:
+			minutes_past_starting_minute = 1
+		if minutes_past_starting_minute % minutes_to_call_notification == 0:
+			emit_signal(("call_time_checker_notification"))
+
+func _on_spin_box_value_changed(value):
+	print("spin box value changed " + str(value))
+	minutes_to_call_notification = value
+	#also reset minutes past time notification.
+	minutes_past_starting_minute = 0
+
+
+func _on_check_button_toggled(toggled_on):
+	print("button toggled, value: " + str(toggled_on))
+	start_at_current_time = toggled_on
+	if toggled_on:
+		minutes_past_starting_minute = 0
--- Scripts/CurrentTime.gd
+++ Scripts/CurrentTime.gd
@@ -1,30 +1,20 @@
 extends Label
 
-@export var minutes_to_call_notification := 1
 var current_time:String
+var minute_at_current_delta: int
 var current_minute: int
-var last_minute: int
 
-signal call_time_checker_notification
-
-func _ready():
-	last_minute = 60
-	print(last_minute)
+signal minute_changed(current_minute :int)
 
 func _process(delta):
 	current_time = Time.get_time_string_from_system(false)
 	text = current_time
 	#current time string is formated like this: HH:MM:SS
-	current_minute = int(text.get_slice(":", 1))
+	minute_at_current_delta = int(text.get_slice(":", 1))
 	#we will need to operate on hours to, so instead of ending at 59, the minutes
 	#should end at 60 and start at 1
-	if current_minute == 0:
-		current_minute = 60
-	if last_minute != current_minute:
-		last_minute = current_minute
-		_on_minute_changed()
-
-func _on_minute_changed():
-	#use % to see if a notification should be going on at this minute
-	if last_minute % minutes_to_call_notification == 0:
-		emit_signal("call_time_checker_notification")
+	if minute_at_current_delta == 0:
+		minute_at_current_delta = 60
+	if current_minute != minute_at_current_delta:
+		current_minute = minute_at_current_delta
+		emit_signal("minute_changed", current_minute)
