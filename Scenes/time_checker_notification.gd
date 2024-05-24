470acfa31a260dd568b42b3bd4fe773865834667
JellyLion <jellylionstudios@gmail.com>
2024-05-24 16:58:08 -0600

    Made windows have notes and ways of submitting the notes

--- Scenes/time_checker_notification.gd
+++ Scenes/time_checker_notification.gd
@@ -1,18 +1,26 @@
 extends Window
 
 var can_close_window:bool = false
+var notification_is_being_used := false
+var current_text : String
 
 func _on_main_call_time_checker_notification():
 	show()
-	can_close_window = true
 	await get_tree().create_timer(5.0).timeout
-	can_close_window = false
-	hide_window_by_timeout()
-	
-func on_button_pressed():
-	if can_close_window:
+	if not notification_is_being_used:
 		hide()
 
-func hide_window_by_timeout():
+func _on_line_edit_text_changed(new_text):
+	#avoid closing notification window
+	notification_is_being_used = true
+	current_text = new_text
+
+func _on_line_edit_text_submitted(new_text):
+	#close notification and save the text.
+	notification_is_being_used = false
+	print("note submitted")	
 	hide()
 
+func _on_button_pressed():
+	if notification_is_being_used:
+		_on_line_edit_text_submitted(current_text)
