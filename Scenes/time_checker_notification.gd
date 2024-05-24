c6dbfe1440943eef0766c86f7de2f2d8da7a0a10
JellyLion <jellylionstudios@gmail.com>
2024-05-24 17:46:01 -0600

    Changed notifications to tell current time

--- Scenes/time_checker_notification.gd
+++ Scenes/time_checker_notification.gd
@@ -4,7 +4,13 @@ var can_close_window:bool = false
 var notification_is_being_used := false
 var current_text : String
 
+#when calling this, send current time in hours and minutes
 func _on_main_call_time_checker_notification():
+	var current_time_text :String = Time.get_time_string_from_system(false)
+	current_time_text = current_time_text.erase(5,3)
+	print(current_time_text)
+	current_time_text = "It's " + current_time_text + "!"
+	$HBoxContainer/VBoxContainer/CurrentTimeHContainer/CurrentTimeLabel.text = current_time_text
 	show()
 	await get_tree().create_timer(5.0).timeout
 	if not notification_is_being_used:
